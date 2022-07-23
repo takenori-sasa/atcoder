f164d99ff35c16b5eb746b5c8b92383f6eb58b66
takenori-sasa <yoshida.tkw@gmail.com>
2022-07-23 19:00:54 +0900

    ♻️ # フォルダ名かえた

--- practice/abs/abc083b/main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from sys import stdin
-# s = stdin.readline().rstrip()
-# a = int(stdin.readline().rstrip())
-# 複数行
-# n = int(stdin.readline().rstrip())
-# data = [stdin.readline().rstrip().split() for _ in range(n)]
