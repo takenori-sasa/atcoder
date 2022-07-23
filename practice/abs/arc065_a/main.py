cca451f4628121c7256d54c597337a96af78578c
takenori-sasa <yoshida.tkw@gmail.com>
2022-07-23 23:53:54 +0900

    👍# abs一問

--- practice/abs/arc065_a/main.py
+++ practice/abs/arc065_a/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from sys import stdin
+import copy
 '''
 s = stdin.readline().rstrip()
 a = int(stdin.readline().rstrip())
@@ -8,3 +9,21 @@ b,c = [int(s) for s in stdin.readline().rstrip().split()]
 n = int(stdin.readline().rstrip())
 data = [stdin.readline().rstrip().split() for _ in range(n)]
 '''
+
+s = stdin.readline().rstrip()
+words = ['dream', 'dreamer', 'erase', 'eraser']
+tt = copy.deepcopy(words)
+ans = 'NO'
+while len(tt) > 0:
+    prevs = copy.deepcopy(tt)
+    tt = []
+    for prev in prevs:
+        for word in words:
+            concats = prev+word
+            if concats == s:
+                # print(concats, s)
+                ans = 'YES'
+                break
+            elif len(concats) <= len(s) and s.startswith(concats):
+                tt.append(concats)
+print(ans)
