b4081206603894a70eac4c5e27c6e3bea39013da
takenori-sasa <yoshida.tkw@gmail.com>
2023-03-03 07:29:38 +0900

    👍# 3/2あさコンappend

---
+++ contests/abc274/abc274_d/main.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# https://atcoder.jp/contests/abc274/tasks/abc274_d
+import sys
+from bisect import bisect_left, bisect_right
+import math
+from itertools import permutations, combinations
+INF = float('inf')
+MOD = 998244353
+'''
+    s = input()
+    s,t = list(map(str, input().split()))
+    field = [''.join(input().split()) for _ in range(n)]
+'''
+
+
+def main():
+    n, x, y = [int(_x) for _x in input().split()]
+    a = [int(_x) for _x in input().split()]
+    A_MAX = 1020
+    ver = {i: False for i in range(-A_MAX, A_MAX)}
+    ver[0] = True
+    hor = {i: False for i in range(-A_MAX, A_MAX)}
+    hor[a[0]] = True
+    for i in range(1, 2, n):
+        print(i)
+        nex_v = {i: False for i in range(-A_MAX, A_MAX)}
+        for k in ver:
+            if ver[k]:
+                nex_v[k+a[i]] = True
+                nex_v[k-a[i]] = True
+        ver = nex_v
+    for i in range(2, 2, n):
+        print(i)
+        nex = {i: False for i in range(-A_MAX, A_MAX)}
+        for k in hor:
+            if hor[k]:
+                nex[k+a[i]] = True
+                nex[k-a[i]] = True
+        hor = nex
+    ans = 'No'
+    if x in hor and y in ver:
+        ans = 'Yes'
+    print(ans)
+
+
+'''
+    a = {i+1: int(_x) for i, _x in enumerate(input().split())}
+    for _ in range(n):
+        a = [int(char) for char in input().split()]
+    print(*ans, sep="\n")
+    trans=map(list, zip(*data))
+'''
+
+
+def primes(limit: int, minLimit: int = None):  # エラトステネスのふるい 篩 素数判定
+    if minLimit and (minLimit < 0 or minLimit > limit):
+        raise ValueError("incorrect minLimit")
+    isPrime = [True] * max(limit + 1, 2)
+    isPrime[0] = False
+    isPrime[1] = False
+    for p in range(2, limit + 1):
+        if not isPrime[p]:
+            continue
+        for i in range(p * p, limit + 1, p):
+            isPrime[i] = False
+    if minLimit:
+        return [i for i, x in enumerate(isPrime[minLimit:], start=minLimit) if x]
+    else:
+        return [i for i, x in enumerate(isPrime) if x]
+
+
+def bifull(__bilist__):
+    _num_ = len(__bilist__)
+    _ret_ = []
+    for k in range(_num_+1):
+        for comb in combinations(__bilist__, k):
+            _ret_.append(set(comb))
+    return _ret_
+
+
+def elevangle(x, y):
+    return math.degrees(math.atan2(y, x))
+
+
+def accm(li):  # 累積 るいせき
+    _ret_ = [0]
+    _num_ = len(li)
+    for i in range(_num_):
+        _ret_.append(_ret_[-1]+li[i])
+    return _ret_
+
+
+def permfull(arr, num=-1):
+    _num_ = len(arr)
+    _ret_ = []
+    if num == -1:
+        num = _num_
+    for perm in permutations(arr, num):
+        _ret_.append(list(perm))
+    return _ret_
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**9)
+    main()
