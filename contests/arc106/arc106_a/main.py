a485acfe70d6a66295cd210084940d5d2c8b540c
takenori-sasa <yoshida.tkw@gmail.com>
2023-03-05 20:55:01 +0900

    👍# 3/5あさコン

---
+++ contests/arc106/arc106_a/main.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# https://atcoder.jp/contests/arc106/tasks/arc106_a
+import sys
+from bisect import bisect_left, bisect_right
+import math
+from itertools import permutations, combinations
+from heapq import heappush, heappop
+from collections import deque
+INF = float('inf')
+MOD = 998244353
+DXY4 = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+DXY8 = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+'''
+    s = input()
+    s,t = list(map(str, input().split()))
+    field = [''.join(input().split()) for _ in range(n)]
+    grid = [[int(_x) for _x in input().split()] for _ in range(n)]
+'''
+
+
+def main():
+    n = int(input())
+    A_MAX = int(18*math.log(10)/math.log(3))
+    B_MAX = int(18*math.log(10)/math.log(5))
+    # print(A_MAX, B_MAX)
+    for i in range(1, A_MAX+1):
+        for j in range(1, B_MAX+1):
+            if pow(3, i)+pow(5, j) == n:
+                print(i, j)
+                return
+    print(-1)
+
+
+'''
+    a = {i+1: int(_x) for i, _x in enumerate(input().split())}
+    for _ in range(n):
+        a = [int(char) for char in input().split()]
+    print(*ans, sep="\n")
+    tenchi = list(zip(*matrix))
+'''
+
+
+# エラトステネスのふるい 篩 素数判定
+def primes(limit: int, minLimit: int = None):
+    if minLimit and (minLimit < 0 or minLimit > limit):
+        raise ValueError("incorrect minLimit")
+    isPrime = [True] * max(limit + 1, 2)
+    isPrime[0] = False
+    isPrime[1] = False
+    for p in range(2, limit + 1):
+        if not isPrime[p]:
+            continue
+        for i in range(p * p, limit + 1, p):
+            isPrime[i] = False
+    if minLimit:
+        return [i for i, x in enumerate(isPrime[minLimit:], start=minLimit) if x]
+    else:
+        return [i for i, x in enumerate(isPrime) if x]
+
+
+def bitfull(__bilist__):  # bit全探索 組み合わせ全探索
+    _num_ = len(__bilist__)
+    _ret_ = []
+    for k in range(_num_+1):
+        for comb in combinations(__bilist__, k):
+            _ret_.append(list(comb))
+    return _ret_
+
+
+def elevangle(x, y):  # 仰角 俯角 ぎょうかく ふかく
+    return math.degrees(math.atan2(y, x))
+
+
+def accm(li):  # 累積和 るいせき
+    _ret_ = [0]
+    _num_ = len(li)
+    for i in range(_num_):
+        _ret_.append(_ret_[-1]+li[i])
+    return _ret_
+
+
+def permfull(arr, num: int = -1):  # 順列全探索
+    _num_ = len(arr)
+    _ret_ = []
+    if num == -1:
+        num = _num_
+    for perm in permutations(arr, num):
+        _ret_.append(list(perm))
+    return _ret_
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**9)
+    input = sys.stdin.readline
+    main()
